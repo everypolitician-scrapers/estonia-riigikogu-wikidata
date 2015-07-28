494d7e6c22d35ddc0fb22933fe952ab36d97b5e3
Tony Bowden <tony@tmtm.com>
2015-07-28 09:54:57 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,11 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+
+WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').wikidata_ids.each do |id|
+  data = WikiData::Fetcher.new(id: id).data('et') or next
+  puts data
+  ScraperWiki.save_sqlite([:id], data)
+end
