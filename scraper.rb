86c56ad3c813117bb41a9d95c520453d2452164c
Tony Bowden <tony@tmtm.com>
2016-01-12 14:43:49 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -4,12 +4,7 @@
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').wikidata_ids.each do |id|
-  data = WikiData::Fetcher.new(id: id).data('et') or next
-  # puts data
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-require 'rest-client'
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { et: names }, output: false)
+warn EveryPolitician::Wikidata.notify_rebuilder
 
