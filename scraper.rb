c2e73aec782552fef09c6b803125c030c6190f59
Tony Bowden <tony@tmtm.com>
2016-01-23 20:16:29 +0000

    Add 12th Riigikogu

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,8 @@
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles
+names = (WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles +
+        WikiData::Category.new('Kategooria:XII_Riigikogu_liikmed', 'et').member_titles).uniq
 EveryPolitician::Wikidata.scrape_wikidata(names: { et: names }, output: false)
 warn EveryPolitician::Wikidata.notify_rebuilder
 
