245074da810f3fe8d789ba573716509d9ac3e1ae
Tony Bowden <tony@tmtm.com>
2016-12-22 08:01:42 +0000

    Scrape new Category
    Kategooria:XIII Riigikogu liikmed, kes Riigikogu töös ei osalenud

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,7 @@ require 'scraperwiki'
 require 'wikidata/fetcher'
 
 names = WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles |
+        WikiData::Category.new('Kategooria:XIII Riigikogu liikmed, kes Riigikogu töös ei osalenud', 'et').member_titles |
         WikiData::Category.new('Kategooria:XII_Riigikogu_liikmed', 'et').member_titles
 EveryPolitician::Wikidata.scrape_wikidata(names: { et: names }, output: false)
 warn EveryPolitician::Wikidata.notify_rebuilder
