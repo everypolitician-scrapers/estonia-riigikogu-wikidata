6787c773ba89701f407552d1cb1c9917ef85691d
Tony Bowden <tony@tmtm.com>
2016-12-29 11:13:46 +0000

    Fetch anyone who's a member of the 13th Riigikogu
    Use SPARQL to get anyone who's set up as a member of the term already
    (and perhaps isn't in the Category, perhaps because they don't have a
    page on et Wikipedia)

--- scraper.rb
+++ scraper.rb
@@ -4,7 +4,19 @@
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
+query = <<-EOQ
+  SELECT DISTINCT ?item {
+    ?item p:P39 ?position_statement .
+    ?position_statement ps:P39 ?position .
+    ?position wdt:P279* wd:Q21100241 .
+    ?position_statement pq:P2937 wd:Q20530392 .
+  }
+EOQ
+
+ids = EveryPolitician::Wikidata.sparql(query)
+
 names = WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles |
         WikiData::Category.new('Kategooria:XIII Riigikogu liikmed, kes Riigikogu töös ei osalenud', 'et').member_titles |
         WikiData::Category.new('Kategooria:XII_Riigikogu_liikmed', 'et').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { et: names }, output: false)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { et: names })
