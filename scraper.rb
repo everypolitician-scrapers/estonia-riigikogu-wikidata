48b1e0710010835f8abcf746f461df07d6025835
Tony Bowden <tony@tmtm.com>
2017-07-27 08:50:39 +0100

    Switch to per-term membership in SPARQL
    We're now using "Member of the 13th Riigikogu" instead of term qualifier
    on "Member of the Estonian Riigikogu"

--- scraper.rb
+++ scraper.rb
@@ -4,15 +4,7 @@
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-query = <<-EOQ
-  SELECT DISTINCT ?item {
-    ?item p:P39 ?position_statement .
-    ?position_statement ps:P39 ?position .
-    ?position wdt:P279* wd:Q21100241 .
-    ?position_statement pq:P2937 wd:Q20530392 .
-  }
-EOQ
-
+query = 'SELECT DISTINCT ?item { ?item wdt:P39 wd:Q33129158 }'
 ids = EveryPolitician::Wikidata.sparql(query)
 
 names = WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles |
