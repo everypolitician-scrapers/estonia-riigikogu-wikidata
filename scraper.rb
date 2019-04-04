78377c19d4352a27f57571b1f697cc0bdad52ad5
Tony Bowden <tony@tmtm.com>
2019-04-04 07:15:08 +0100

    Fetch entirely from P39s
    There is nothing included in the Wikipedia categories that doesn't
    already exist in P39s

--- scraper.rb
+++ scraper.rb
@@ -4,11 +4,13 @@
 require 'scraperwiki'
 require 'wikidata/fetcher'
 
-query = 'SELECT DISTINCT ?item { ?item wdt:P39 wd:Q33129158 }'
-ids = EveryPolitician::Wikidata.sparql(query)
+sparq = <<EOQ
+  SELECT DISTINCT ?item WHERE {
+    ?item p:P39/ps:P39 [ wdt:P279 wd:Q21100241 ; wdt:P2937 ?term ] .
+    ?term p:P31/pq:P1545 ?ordinal .
+    FILTER (xsd:integer(?ordinal) >= 12)
+  }
+EOQ
+ids = EveryPolitician::Wikidata.sparql(sparq)
 
-names = WikiData::Category.new('Kategooria:XIII_Riigikogu_liikmed', 'et').member_titles |
-        WikiData::Category.new('Kategooria:XIII Riigikogu liikmed, kes Riigikogu töös ei osalenud', 'et').member_titles |
-        WikiData::Category.new('Kategooria:XII_Riigikogu_liikmed', 'et').member_titles
-
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { et: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
